a0a0acd62a819489374a42ecd8b4b0975eeb6684
Dejan Mrkela <mrkeladejan@gmail.com>
2021-12-18 14:23:38 +0100

    info update i 1.nivo

--- g/Name_Generator/00.py
+++ g/Name_Generator/00.py
@@ -1,5 +1,12 @@
-#Name
 import names
+import random
+
+#nivo1.
 print(names.get_full_name())
 
+#nivo2.
+a = ["A+","A-","B+","B-","AB+","AB-","O+","O-"] 
+print("Visina:", random.randrange(130, 220), "cm")
+print("Težina:", random.randrange(50, 150), "Kg")
+print("Krvna grupa:", random.choice(a))
 

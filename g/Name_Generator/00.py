0ba1b9e16a3594385656e89f396c9e8ee1eba2da
Dejan Mrkela <mrkeladejan@gmail.com>
2021-12-18 15:05:29 +0100

    nivo.3

--- g/Name_Generator/00.py
+++ g/Name_Generator/00.py
@@ -1,12 +1,23 @@
 import names
 import random
+import datetime
 
 #nivo1.
-print(names.get_full_name())
+ime = input("Izaberite pol, m za muški, f za ženski: ")
+if ime == "m":
+    print("\n  ",names.get_full_name(gender='male'))
+elif ime == "f":
+    print("\n  ",names.get_full_name(gender='female'))
+else:
+    print("Došlo je do greške.")
+    quit()
 
 #nivo2.
-a = ["A+","A-","B+","B-","AB+","AB-","O+","O-"] 
-print("Visina:", random.randrange(130, 220), "cm")
-print("Težina:", random.randrange(50, 150), "Kg")
-print("Krvna grupa:", random.choice(a))
+print("Fizičke karakteristike:")
+krv = ["A+","A-","B+","B-","AB+","AB-","O+","O-"] 
+print(" Visina:", random.randrange(130, 220), "cm")
+print(" Težina:", random.randrange(50, 150), "Kg")
+print(" Krvna grupa:", random.choice(krv))
+
+#nivo3.
 

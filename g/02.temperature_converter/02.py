50f3c10981f68856364ccd3412e25d62687ec300
Dejan Mrkela <mrkeladejan@gmail.com>
2021-12-21 13:23:07 +0100

    nivo2. done

--- g/02.temperature_converter/02.py
+++ g/02.temperature_converter/02.py
@@ -1,4 +1,16 @@
 #nivo1.
-f = input("Uneti temperaturu u farenhajtima: ")
-c = (int(f) - 32) * (5/9)
-print("Uneta temperatura u celzijusima je", c)
+#f = input("Uneti temperaturu u farenhajtima: ")
+#c = (int(f) - 32) * (5/9)
+#print("Uneta temperatura u celzijusima je", c)
+
+#nivo2.
+k = input("Izabrati pocetni sistem, uneti f za farenhajt, c celzijus: ")
+temp = input("Uneti brojnu vrednost: ")
+if k == "f":
+    c = (int(temp) - 32) * (5/9)
+    print("Temperatura u celzijusu je:", c)
+elif k == "c":
+    f = (int(temp) * (5/9)) + 32
+    print("Temperatura u farenhajtu je:", f)
+else:
+    print("Doslo je do greske.")
